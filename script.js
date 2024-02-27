b5729eedbaed062fbb5a97218cc0b4d9608229e8
Eric Moore <ericmoore0709@gmail.com>
2024-02-26 16:04:44 -0800

    added functionality

--- script.js
+++ script.js
@@ -4,24 +4,35 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
-	let results = [];
-
-	// TODO
-
-	return results;
+	if (str)
+		return fruit.filter((x) => x.toLowerCase().includes(str.toLowerCase()));
 }
 
 function searchHandler(e) {
+	console.log("searchHandler");
 	// TODO
+	showSuggestions(search(e.target.value), e.target.value);
 }
 
 function showSuggestions(results, inputVal) {
-
+	console.log("showSuggestions");
 	// TODO
+	while (suggestions.lastChild)
+		suggestions.removeChild(suggestions.lastChild);
+
+	search(inputVal).forEach((x) => {
+		const li = document.createElement('li');
+		li.textContent = x;
+		suggestions.appendChild(li);
+	});
 }
 
 function useSuggestion(e) {
+	console.log("useSuggestion");
 	// TODO
+	input.value = e.target.textContent;
+	while (suggestions.lastChild)
+		suggestions.removeChild(suggestions.lastChild);
 }
 
 input.addEventListener('keyup', searchHandler);
